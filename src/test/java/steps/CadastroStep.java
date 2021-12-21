24917489ee1b6f29028bba496a9539b0cd8f73a6
jeferson-learn <jefynhuwork@gmail.com>
2021-12-21 10:15:40 -0300

    Adicionei metodo para validar

--- src/test/java/steps/CadastroStep.java
+++ src/test/java/steps/CadastroStep.java
@@ -1,21 +1,21 @@
 package steps;
 
-import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
 import pages.CadastroPage;
-import pages.LoginPage;
 import runner.RunCucumberTest;
 
 public class CadastroStep extends RunCucumberTest {
 
     CadastroPage cadastroPage = new CadastroPage(driver);
+    String nome = "Jeferson";
+    String sobrenome = "Eugenio";
 
     @Quando("^eu preencho o formulário de cadastro$")
     public void eu_preencho_o_formulário_de_cadastro() {
         cadastroPage.selectTitle(1);
-        cadastroPage.preencheNome("Jeferson");
-        cadastroPage.preencheSobrenome("Lopes");
+        cadastroPage.preencheNome(nome);
+        cadastroPage.preencheSobrenome(sobrenome);
         cadastroPage.preencheSenha("123456");
         cadastroPage.selecionaAniversario(3, 11, "1992");
         cadastroPage.preencheEndereco("Setor 1 Quadra N");
@@ -34,7 +34,7 @@ public class CadastroStep extends RunCucumberTest {
 
     @Então("^vejo cadastro realizado com sucesso$")
     public void vejo_cadastro_realizado_com_sucesso() {
-
+        cadastroPage.validaCadastro(nome, sobrenome);
     }
 
 
